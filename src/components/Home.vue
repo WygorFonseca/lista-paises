2447d8c300d064f95a379b49c83e2d596b046e31
unknown <wygorfonseca@hotmail.com>
2020-12-01 16:01:47 -0300

    Início projeto

---
+++ src/components/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container h-100">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './main.scss';
+// import axios from 'axios';
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  mounted: function () {
+    // axios.get('https://restcountries.eu/rest/v2/all')
+    // .then(({ data }) => {
+    //   console.log(data);
+    // })
+    // .catch(error => {
+    //   console.log(error);
+    // })
+  }
 }).$mount('#app')
